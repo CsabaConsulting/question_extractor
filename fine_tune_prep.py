bab2cc9e84710d5e5cdc5db0bb567671fcdc48c0
MrCsabaToth <csaba.toth.us@gmail.com>
2023-09-11 18:46:14 -0700

    OpenAI fine tune actually requires JSONL instead of JSON. Ref https://stackoverflow.com/questions/38915183/python-conversion-from-json-to-jsonl

--- fine_tune_prep.py
+++ fine_tune_prep.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Define the input and output paths
 input_filepath = Path('./data/questions.json')
-output_filepath = Path('./data/fine_tune.json')
+output_filepath = Path('./data/fine_tune.jsonl')
 system_prompt = \
     "Casey is your assistant specialized in ThruThink budgeting analysis and projection web application usage. " + \
     "It is also knowledgeable in a wide range of budgeting and accounting topics, including EBITDA, " + \
@@ -17,16 +17,18 @@ system_prompt = system_prompt.replace("\n", " ").strip()
 # Expecting the questions.json with an array of { source, question, answer } pair tuples.
 with open(input_filepath, 'r') as input_file:
     input_json = json.load(input_file)
-    output_tuples = []
-    for input_tuple in input_json:
-        output_tuples.append([
-            {'role': 'system', 'content': system_prompt},
-            {'role': 'user', 'content': input_tuple['question']},
-            {'role': 'assistant', 'content': input_tuple['answer']}
-        ])
-
-    output_json = {'messages': output_tuples}
     # Save the extracted questions as a JSON file
     with open(output_filepath, 'w') as output_file:
-        json.dump(output_json, output_file, indent=2)
+        for input_tuple in input_json:
+            qna = {
+                'messages': [
+                    {'role': 'system', 'content': system_prompt},
+                    {'role': 'user', 'content': input_tuple['question']},
+                    {'role': 'assistant', 'content': input_tuple['answer']}
+                ]
+            }
+
+            json.dump(qna, output_file)
+            output_file.write('\n')
+
         print(f"Results have been saved to {output_filepath}.")
