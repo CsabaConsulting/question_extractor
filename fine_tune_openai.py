f632a5e566aca2fbdf1e20496e322285f2a87f92
MrCsabaToth <csaba.toth.us@gmail.com>
2023-11-07 14:06:00 -0800

    Rename fine tune for OpenAI (bc AnyScale is coming)

--- fine_tune_openai.py
+++ fine_tune_openai.py
@@ -3,7 +3,7 @@ import os
 import openai
 
 # Define the fine tune file path
-input_filepath = Path('./data/fine_tune.jsonl')
+input_filepath = Path('./data/fine_tune_openai.jsonl')
 epochs = 3
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -14,7 +14,7 @@ upload_response = openai.File.create(
 file_id = upload_response['id']
 print(f"Fine tune file {input_filepath} has been uploaded with id {file_id}.\n")
 
-tuning_response = openai.FineTuningJob.create(training_file=file_id, model="gpt-3.5-turbo")
+tuning_response = openai.FineTuningJob.create(training_file=file_id, model="gpt-3.5-turbo-1106")
 org_id = tuning_response["organization_id"]
 tune_model = tuning_response["model"]
 tune_id = tuning_response["id"]
