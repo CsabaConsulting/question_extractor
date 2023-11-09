8eb83bbbf129dca8db778472dddb6b69ea6d3f58
MrCsabaToth <csaba.toth.us@gmail.com>
2023-11-09 00:26:30 -0800

    Being able to continue pt 2: saving and not regenerating result

--- question_extractor/__init__.py
+++ question_extractor/__init__.py
@@ -275,18 +275,26 @@ async def process_file(file_path, text, progress_counter, verbose=True, parallel
         with open(questions_file_name, 'w') as output_file:
             json.dump(questions, output_file, indent=2)
 
-    # Build and run answering tasks concurrently
-    tasks = []
-    for sub_file_path, sub_text, question in questions:
-        task = generate_answer(question, sub_text)
-        tasks.append(task)
-
-    tasks_outputs = await asyncio.gather(*tasks)
-
-    # Merge results into a list of dictionaries
+    results_filename = f"{file_path}_result.json"
     result = []
-    for (sub_file_path, sub_text, question), answer in zip(questions, tasks_outputs):
-        result.append({'source': sub_file_path, 'question': question, 'answer': answer})
+    if Path(results_filename).is_file():
+        with open(results_filename, 'r') as input_file2:
+            result = json.loads(input_file2.read())
+    else:
+        # Build and run answering tasks concurrently
+        tasks = []
+        for sub_file_path, sub_text, question in questions:
+            task = generate_answer(question, sub_text)
+            tasks.append(task)
+
+        tasks_outputs = await asyncio.gather(*tasks)
+
+        # Merge results into a list of dictionaries
+        for (sub_file_path, sub_text, question), answer in zip(questions, tasks_outputs):
+            result.append({'source': sub_file_path, 'question': question, 'answer': answer})
+
+        with open(results_filename, 'w') as output_file2:
+            json.dump(questions, output_file2, indent=2)
 
     # Update progress and display information if verbose is True
     progress_counter['nb_files_done'] += 1  # No race condition as we are single-threaded
